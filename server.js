f5ed3d4f149d3ea0ab96ffbf52fa821fff6b1ab6
James Morgan <jmorga11@gmail.com>
2019-02-08 13:08:00 -0800

    Added Projects Page

--- server.js
+++ server.js
@@ -49,7 +49,13 @@ app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
   });
-})
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
 
 app.get('/bad', (req, res) => {
   res.send({
